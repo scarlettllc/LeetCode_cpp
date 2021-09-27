f504aa0f99d18ccb6b8e02961b576655b4cf9acc
scarlett <liangchuanluo@gmail.com>
2021-09-27 19:52:07 +0800

    929_Unique_Email_Addressed first_solution

---
+++ 929_Unique_Email_Addresses/first_solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<string> s;
+        int n = emails.size();
+        for (int i = 0; i < n; ++i) {
+            string email = emails[i];
+            int pos = 0;
+            while (pos < email.size()) {
+                if (email[pos] == '@') {
+                    break;
+                }
+                ++pos;
+            }
+            string right_part = email.substr(pos);
+            string left_part = email.substr(0, pos);
+            pos = -1;
+            for (int j = 0; j < left_part.size(); ++j) {
+                if (email[j] == '+') {
+                    pos = j;
+                    break;
+                }
+            }
+            if (pos != -1) {
+                left_part = left_part.substr(0, pos);
+            }
+            string final_left_part = "";
+            for (int j = 0; j < left_part.size(); ++j) {
+                if (left_part[j] != '.') {
+                    final_left_part += left_part[j];
+                }
+            }
+            string equal_email = final_left_part + right_part;
+            if (!s.count(equal_email)) {
+                s.insert(equal_email);
+            }
+        }
+        return s.size();
+    }
+};
