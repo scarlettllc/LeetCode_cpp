bd2dde9423a39da52d6605e2f863fc936e539064
scarlett <liangchuanluo@gmail.com>
2021-10-27 09:09:59 +0800

    22 Generate Parentheses

---
+++ 22_Generate_Parentheses/official_solution_adjusted.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current;
+        backtrack(result, current, 0, 0, n);
+        return result;
+    }
+    void backtrack(vector<string>& result, string& current, int open, int close, int& n) {
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+        if (close < open) {
+            current.push_back(')');
+            backtrack(result, current, open, close + 1, n);
+            current.pop_back();
+        }
+        if (open < n) {
+            current.push_back('(');
+            backtrack(result, current, open + 1, close, n);
+            current.pop_back();
+        }
+    }
+};
