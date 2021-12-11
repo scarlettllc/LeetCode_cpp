d4bc83e09be0ac63a0e6760b6fd18444dc429f66
scarlett <liangchuanluo@gmail.com>
2021-12-11 12:17:55 +0800

    911 Online Election

--- 911_Online_Election/official_solution.cpp
+++ 911_Online_Election/official_solution.cpp
@@ -28,3 +28,8 @@ public:
  * TopVotedCandidate* obj = new TopVotedCandidate(persons, times);
  * int param_1 = obj->q(t);
  */
+
+比你的代码好的地方：
+1、用的是upper_bound。
+2、直接记录下标对应的top，其实并不需要借助time map。
+3、记录了上一个top，这使得比较的使用大于等于可以避免遍历。
