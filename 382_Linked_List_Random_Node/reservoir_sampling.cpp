781467b5253bf03dda1f73a7cc92bf6b09ebc112
scarlett <liangchuanluo@gmail.com>
2022-01-16 11:43:15 +0800

    382 Linked List Random Node

---
+++ 382_Linked_List_Random_Node/reservoir_sampling.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* head;
+public:
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    int getRandom() {
+        int result = head->val, i = 1;
+        for (ListNode* node = head; node != nullptr; node = node->next) {
+            if (rand() % i == 0) {
+                result = node->val;
+            }
+            ++i;
+        }
+        return result;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
