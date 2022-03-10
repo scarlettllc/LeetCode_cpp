1bdab0f5b05859922f775e62f9c3e2753f97f140
scarlett <liangchuanluo@gmail.com>
2022-03-10 10:10:44 +0800

    589 N-ary Tree Preorder Traversal

---
+++ 589_N-ary_Tree_Preorder_Traversal/own_solution.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+    void dfs(Node* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        result.push_back(root->val);
+        for (auto & node : root->children) {
+            if (node != nullptr) {
+                dfs(node, result);
+            }
+        }
+    }
+};
