b208fde39b21e441127a3f000b9171bd916b5584
scarlett <liangchuanluo@gmail.com>
2022-06-29 19:14:42 +0800

    535 Encode and Decode TinyURL

---
+++ 535_Encode_and_Decode_TinyURL/official_solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    unordered_map<int, string> dataBase;
+    int id;
+
+public:
+    Solution() {
+        id = 0;
+    }
+
+    string encode(string longUrl) {
+        id++;
+        dataBase[id] = longUrl;
+        return string("http://tinyurl.com/") + to_string(id);
+    }
+
+    string decode(string shortUrl) {
+        int p = shortUrl.rfind('/') + 1;
+        int key = stoi(shortUrl.substr(p, int(shortUrl.size()) - p));
+        return dataBase[key];
+    }
+};
